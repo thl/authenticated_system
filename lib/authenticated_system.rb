781d8b54ea9a77e99d7ef8bf401d3edd8829407f
Andres Montano <amontano@virginia.edu>
2014-02-25 14:45:28 -0600

    dealing with apache returning ASCII-8BIT instead of UTF-8 in server

--- lib/authenticated_system.rb
+++ lib/authenticated_system.rb
@@ -79,7 +79,7 @@ module AuthenticatedSystem
     #
     # We can return to this location by calling #redirect_back_or_root.
     def store_location
-      session[:return_to] = request.fullpath
+      session[:return_to] = request.fullpath.dup.force_encoding(Encoding::UTF_8)
     end
 
     # Redirect to the URI stored by the most recent store_location call or
@@ -117,15 +117,15 @@ module AuthenticatedSystem
     end
     
     def shibboleth_id
-      request.env['REMOTE_USER']#.blank? ? session[:netbadgeid] : request.env['REMOTE_USER']
+      request.env['REMOTE_USER'].dup.force_encoding(Encoding::UTF_8)#.blank? ? session[:netbadgeid] : request.env['REMOTE_USER']
     end
     
     def shibboleth_fullname
-      "#{request.env['givenName']} #{request.env['sn']}"
+      "#{request.env['givenName'].dup.force_encoding(Encoding::UTF_8)} #{request.env['sn'].dup.force_encoding(Encoding::UTF_8)}"
     end
 
     def shibboleth_email
-      request.env['eppn']
+      request.env['eppn'].dup.force_encoding(Encoding::UTF_8)
     end
 end
 
